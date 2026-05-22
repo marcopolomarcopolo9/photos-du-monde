c8255653146fd80bada55075615ab44f90ac4f34
marcopolomarcopolo9 <marcetter983@gmail.com>
2026-05-22 08:18:40 +0200

    Admin: Modification voyage costa rica & jungl

--- lib/data.ts
+++ lib/data.ts
@@ -25,9 +25,10 @@ export const VOYAGES = [
     city: "San josé ",
     startDate: "2024",
     description: "Un voyage a la carte au millieu de jungle et plage un savoureux melange de couleure et ambiances..",
-    heroImage: "",
+    heroImage: "https://res.cloudinary.com/doxsjisyx/image/upload/v1779430677/photos-du-monde/zoamp6ljimhdi3uykrfp.jpg",
     photos: [
-
+    { src: "https://res.cloudinary.com/doxsjisyx/image/upload/v1779430695/photos-du-monde/iaoklbpf7acmnciznc47.jpg" },
+    { src: "https://res.cloudinary.com/doxsjisyx/image/upload/v1779430697/photos-du-monde/gyaenlhdxfzh7snq8pk3.jpg" }
     ],
     lat: 9.915285,
     lng: -84.105542,
